bf3d958eab41237fc78ca87f5e547b559453fdf8
hackcheek <h4ckcheek@gmail.com>
2022-08-22 15:40:44 -0300

    Spark manager

---
+++ src/session.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.utils import AnalysisException
+
+
+def spark_session() -> SparkSession:
+
+    if SparkSession.getActiveSession():
+        return SparkSession.getActiveSession()
+
+    else:
+        spark: SparkSession = (
+            SparkSession
+            .builder
+            .master("local")
+            .appName("CO2_Emissions")
+            .config('spark.driver.host', '127.0.0.1')
+            .config('spark.driver.bindAddress', '127.0.0.1')
+            .config("spark.ui.port", "4040")
+            .getOrCreate()
+        )
+        return spark
+
+
+def updata(spark: SparkSession, path: str) -> DataFrame:
+    """
+    Usage
+    >>> spark = spark_session
+    >>> df = updata(spark, 'datasets/data.csv')
+    """
+
+    data: DataFrame = spark.read.csv(
+        path,
+        header=True,
+    )
+
+    try:
+        spark.sql("SELECT * FROM data")
+
+    except AnalysisException:
+        data.registerTempTable("data")
+
+    return data
