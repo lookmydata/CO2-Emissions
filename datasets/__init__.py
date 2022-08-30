9d5b3ea898698f0e19a5a823fecbff38125c7ce5
hackcheek <h4ckcheek@gmail.com>
2022-08-30 12:20:53 -0300

    update etl

---
+++ datasets/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+DATASETS_PATH: Path = Path(__file__).parent
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CO2-Emissions',
+    version='0.1',
+    description='Data product que busca, a partir de notorios analisis y modelos de datos, ayudar a los paises a reducir su emision de dioxido de carbono',
+    url='https://github.com/lookmydata/CO2-Emissions',
+    author='LookMyData',
+    author_email='h4ckcheek@gmail.com',
+    license='MIT',
+    packages=['src'],
+    zip_safe=False
+)
