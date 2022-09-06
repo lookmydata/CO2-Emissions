b6159830bfb4e807384cac7feb879d408c708c2c
luqui5 <lucas12345sanchez6@gmail.com>
2022-09-06 15:53:41 -0300

    lucas luqui.py asd

--- pipeline/norm_enfermedades.py
+++ pipeline/norm_enfermedades.py
@@ -32,3 +32,25 @@ def norm_enfermedades(dfm,dff):
     df_final = pd.merge(dff,dfm,how='outer',on=['pais','anio'])
     
     return df_final
+
+def E_enf(dff):
+    def cambiaraint (dataframe):
+        i = 0
+        for i in range(len(dataframe)):
+            if type(dataframe['casos'][i]) == str:
+                if "k" in dataframe['casos'][i]:
+                    dataframe['casos'][i] = round(float(dataframe['casos'][i].split("k")[0])*1000)
+            dataframe['casos'][i] = round(float(dataframe['casos'][i]))
+            i = i+1
+        return dataframe
+    dff = dff.fillna(0)
+
+    dff = dff.rename(columns={'country' :'pais'})
+    
+    dff = dff.melt(id_vars='pais', var_name='anio', value_name='casos')
+
+    dff = cambiaraint(dff)
+
+    dff = dff.rename(columns={'casos' :'casos F'})
+    
+    return dff
