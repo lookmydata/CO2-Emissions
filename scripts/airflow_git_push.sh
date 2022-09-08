a7c57662bc8d78a0cc54b64c1c418a250de47a95
hackcheek <h4ckcheek@gmail.com>
2022-09-08 14:08:40 -0300

    git flow

--- scripts/airflow_git_push.sh
+++ scripts/airflow_git_push.sh
@@ -1,6 +1,9 @@
+git checkout dev
 git datasets/normalized_delta/*
 git commit -m ".: AIRFLOW UPDATE $(date +'%d-%m-%Y') :."
 git push origin dev
 git add src/api/v2/*
 git commit -m ".: AIRFLOW UPDATE $(date +'%d-%m-%Y') :."
-git push origin gh-pages
+git push origin dev
+git checkout gh-pages
+git merge origin/dev
